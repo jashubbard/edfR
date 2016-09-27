799eacba8286db97bb3360b540ab3708deef7a17
Jason Hubbard <jashubbard@gmail.com>
2016-09-27 13:29:14 -0700

    fixed tests

--- tests/testthat/test_batch_trials.R
+++ tests/testthat/test_batch_trials.R
@@ -15,7 +15,7 @@ test_that("testing edf.trials", {
 
   tr <- edf.trials(edf.data('edf'))
   expect_is(tr,'list')
-  expect_true(all(names(tr) %in% c('headers','fixations','saccades','blinks','messages')))
+  expect_true(all(names(tr) %in% c('header','fixations','saccades','blinks','messages')))
   expect_true('eyetrial' %in% names(tr$fixations))
   expect_gt(length(tr$fixations),0)
 
@@ -27,7 +27,7 @@ test_that("loading batch and combine.eyedata", {
   alldata <- edf.batch(edf.data('edf'),do.plot = F)
   expect_is(alldata,'list')
   expect_equal(length(alldata),length(edf.data('edf')))
-  expect_equal(names(alldata[[1]]),c( "headers","fixations", "saccades", "blinks", "messages", "ID", "filename","header"))
+  expect_equal(names(alldata[[1]]),c( "header","fixations", "saccades", "blinks", "messages", "ID", "filename"))
 
   comb <- combine.eyedata(alldata)
   expect_is(comb$fixations,'data.frame')
